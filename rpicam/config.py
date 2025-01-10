1e7b2b15c8f3dae9e0f38a23af88dbdcdb5005c8
Hakan Kucukdereli <hkucukdereli@gmail.com>
2025-01-10 16:28:54 -0500

    remove old files

--- rpicam/config.py
+++
@@ -1,7 +0,0 @@
-import yaml
-import logging
-from pathlib import Path
-
-def load_config(config_path='camera_config.yaml'):
-    with open(config_path, 'r') as file:
-        return yaml.safe_load(file)
